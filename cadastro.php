ba93752a44fb349b7dcc31dd0cb899d3de76c4db
CristinaJuhee <juhee940@gmail.com>
2015-03-18 09:22:58 -0300

    Update cadastro.php

--- cadastro.php
+++ cadastro.php
@@ -1,44 +1,3 @@
-<DOCTYPE! html>
-<html>
-<head>
-<title>Usuario</title>
-
-<meta http-equiv="content-type" content="text/html; charset="UTF-8">
-
-<body>
-<p>Login</p>
-
-<form action="usuario" method="get">
-
-
-Email:<br>
-<input type="text" name="email" size="20" placeholder="E-MAIL"></input><br>
-
-SENHA:<br>
-<input type="password" name="senha" size="20" /><br>
-
-
-
-
-<input type="submit" value="Log in">
-
-<script language="javascript">
-function check(form)
-{
-
- if(form.email.value == "myemail" && form.senha.value == "mysenha")
-  {
-    window.open('home.html')
-  }
- else
- {
-   alert("Errado. Digite de novo por favor.")
-  }
-}
-
-</body>
-</html>
-
 
 <!DOCTYPE html>
 <html>
@@ -68,40 +27,4 @@ function check(form)
     
     
     
-    <DOCTYPE! html>
-<html>
-<head>
-<title>Usuario</title>
-
-
-
-<meta http-equiv="contento-type" content="text/html; charset="UTF-8">
-
-<body>
-<p>Login</p>
-
-<form action="usuario" method="get">
-
-NOME:<br>
-<input type="text" name="nome" size="20" placeholder="NOME"></input><br>
-
-Email:<br>
-<input type="text" name="email" size="20" placeholder="E-MAIL"></input><br>
-
-SENHA:<br>
-<input type="password" name="senha" size="20" /><br>
-
-CONFIRMAR:<br>
-<input type="password" name="confirmarsenha" size="20" /><br>
-
-<p>SEXO:</p>
-<input type="radio" name="sexo" value="feminino"/>Feminino<br>
-<input type="radio" name="sexo" value="masculino"/>Masculino<br>
-<br></br>
-
-<input type="submit" value="Acesso">
-
-
-
-</body>
-</html>
+  
